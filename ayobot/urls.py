be0fa6a654cd0e0e6d3016c7fdc6ac4a1105fe45
git-freelance <git-freelance@gmail.com>
2024-10-31 20:58:57 +0530

    fix url for static files

--- ayobot/urls.py
+++ ayobot/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin2024/', admin.site.urls),
     path('', include("whatsapp_bot.urls"))
 ]
 
